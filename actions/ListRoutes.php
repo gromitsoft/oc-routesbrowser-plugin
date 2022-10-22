23d4ff391961c38aab40d9eed0f209c3759a49d6
m49n <akpomah@gmail.com>
2022-10-22 20:54:48 +0300

    Update ListRoutes.php

--- actions/ListRoutes.php
+++ actions/ListRoutes.php
@@ -51,7 +51,7 @@ class ListRoutes
     private function isSystemRoute(Route $route): bool
     {
         return Str::startsWith($route->uri(), [
-            str_replace('/','',Config::get('backend.uri')),
+            str_after(Config::get('backend.uri'),'/'),
             'combine',
             'resize',
             '{slug?}',
